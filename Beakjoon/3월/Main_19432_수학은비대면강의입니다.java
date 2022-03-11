2eb3f05878bb0bae9de21562aa37ce23d8b10080
an7188 <an92818@gmail.com>
2022-03-12 00:26:57 +0900

    0312

---
+++ Beakjoon/3월/Main_19432_수학은비대면강의입니다.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_19432_수학은비대면강의입니다 {
+
+	static int a,b,c,d,e,f;
+	
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		f = Integer.parseInt(st.nextToken());
+
+		for (int x = -999; x < 1000; x++) {
+			for (int y = -999; y < 1000; y++) {
+				if((a*x)+(b*y) ==c){
+					if((d*x)+(e*y)==f) {
+						System.out.printf("%d %d ",x,y);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+}
