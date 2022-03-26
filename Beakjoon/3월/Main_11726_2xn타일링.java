fafcbf2ab0f58fbf5aa00b43a416bf5dbc30935f
an7188 <an92818@gmail.com>
2022-03-27 02:07:17 +0900

    0327

---
+++ Beakjoon/3월/Main_11726_2xn타일링.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main_11726_2xn타일링 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[]= new int[n+2];
+        arr[1]=1;
+        arr[2]=2;
+            for (int i = 3; i <= n; i++) {
+                arr[i]=(arr[i-1]+arr[i-2])%10007;
+            }
+            System.out.println(arr[n]%10007);
+
+    }
+}
